bb8a6585c95d02addcc4dc861a71d18211cceac9
qxlx <qxlxi@qq.com>
2024-11-10 21:31:51 +0800

    update

--- qxlx-test/src/main/java/com/qxlx/ss/Test02.java
+++ qxlx-test/src/main/java/com/qxlx/ss/Test02.java
@@ -14,6 +14,7 @@ public class Test02 {
 		// 1,如何读取配置文件,获取IO文件
 		// 2,加载文件后 如何以对象的形式封装
 		// 3.根据配置信息创建对象
+
 		// 4.所创建对象的生命周期
 
 		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("beans.xml");
